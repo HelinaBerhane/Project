034f12b0188318094c971a77b8b5417302abc5c7
Helina Berhane <helina.berhane@outlook.com>
2016-12-01 16:59:30 +0000

    initial output test - working

---
+++ 1_spins/spins.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*    for(int i = 1, i < 10, i = 1 + 1){
+        cout << "i = " << i << endl;
+    }
+*/
+	std::cout << "test" << endl;
+	return 0;
+}
