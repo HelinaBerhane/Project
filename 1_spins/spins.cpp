7be6ef703234020ce06dd70691667048d8162863
Helina Berhane <helina.berhane@outlook.com>
2016-12-04 15:49:53 +0000

    separated functions - working

--- 1_spins/spins.cpp
+++ 1_spins/spins.cpp
@@ -4,34 +4,36 @@
 
 using namespace std;
 
-/*
-void do_array_things( int array[10] ) {
-    cout << array[0] << endl;
-}
-*/
-
-int main(){
+void random_seed(){
     srand(time(NULL));
-    int lattice [10][10];
+}
+
+void generate_lattice(int array[10][10]){
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             int ran = (rand() % 2)*2 - 1;
-            lattice[i][j] = ran;
+            array[i][j] = ran;
+        }
+    }
+}
+
+void print_lattice(int array[10][10]){
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
             cout.width(3);
-            cout << lattice [i][j];
+            cout << array[i][j];
         }
         cout << endl;
     }
-    /*
-    int array[10];
-    do_array_things( array );
-    */
+}
+
+int main(){
+    int lattice [10][10];
+    random_seed();
+    generate_lattice(lattice);
+    print_lattice(lattice);
 }
 
 /*
-
-make separate functions for each task and take them out of main
-
 fix the randomisation using http://stackoverflow.com/questions/19665818/generate-random-numbers-using-c11-random-library
-
 */
