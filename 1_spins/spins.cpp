1fc39ca517fafb7506b1cb8fe3cac0eeb96e01b0
Helina Berhane <helina.berhane@outlook.com>
2016-12-04 15:32:56 +0000

    randomise spins on the lattice - working

--- 1_spins/spins.cpp
+++ 1_spins/spins.cpp
@@ -1,26 +1,37 @@
-#include <iostream>
-#include <string>
+#include <cstdlib>  //rand, srand
+#include <iostream> //cout
+#include <ctime>    //time(NULL)
+
 using namespace std;
 
+/*
+void do_array_things( int array[10] ) {
+    cout << array[0] << endl;
+}
+*/
+
 int main(){
-    string lattice [10][10];
-    for(int i = 1; i < 11; i = i + 1){
-        for(int j = 1; j < 11; j = j + 1){
-            /*
-            string a = "i";
-            string b = to_string(i);
-            string c = "j";
-            string d = to_string(j);
-            cout << a << b << c << d << endl;
-            */
-            lattice[i-1][j-1] = "i" + to_string(i) + "j" + to_string(j);
-            cout << lattice [i-1][j-1] << endl;
+    srand(time(NULL));
+    int lattice [10][10];
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            int ran = (rand() % 2)*2 - 1;
+            lattice[i][j] = ran;
+            cout.width(3);
+            cout << lattice [i][j];
         }
+        cout << endl;
     }
-    /*    
-    int testint = 1;
-    string test = "test";
-    string teststr = test + to_string(testint);
-    cout <<  teststr << endl;
+    /*
+    int array[10];
+    do_array_things( array );
     */
 }
+
+/*
+
+make separate functions for each task and take them out of main
+
+fix the randomisation using http://stackoverflow.com/questions/19665818/generate-random-numbers-using-c11-random-library
+
+*/
