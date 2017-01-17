398431f589c66706cae81f394cd27af72bb787dd
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-01-17 15:52:47 +0000

    improved accuracy - working

--- 2_scalar_exponential/exponentials.cpp
+++ 2_scalar_exponential/exponentials.cpp
@@ -1,21 +1,14 @@
-/* Exponentials */
-// Started at 1:45
-
 #include <iostream>     // cout
 #include <stdlib.h>     // srand, rand
 #include <time.h>       // time
-//#include <gmd.h>        // LaGenMatDouble
-//#include <blas3pp.h>
 using namespace std;
 
 void random_seed(){
     srand(time(NULL));
 }
-
 int ran(int x){
     return rand() % x;
 }
-
 int fact(int x){
 	if(x<=1){
         return 1;
@@ -23,7 +16,6 @@ int fact(int x){
         return x*fact(x-1);
 	}
 }
-
 long double powers(long long int x, long long int y){
     if(y<=1){
         return x;
@@ -33,83 +25,81 @@ long double powers(long long int x, long long int y){
     }
 }
 
-long double expo(long long int matrix, long long int iterations, long long int n){
+/* Redundant */
+long double straight_exponential(long long int number, long long int iterations, int n){
     if(iterations == 1){
-        iterations--;
-        return powers(matrix, n) / fact(n);
+        return powers(number, n) / fact(n);
     }else{
-        cout << powers(matrix, n) << ", " << fact(n) << endl;
-        return powers(matrix, n) / fact(n) + expo(matrix, iterations-1, n+1);
+        return powers(number, n) / fact(n) + straight_exponential(number, iterations-1, n+1);
     }
     /*
     float partial_result = 0;
     float result = 0;
-    // over each iteration, calculate \sum_i{matrix^n/n!}        
+    // over each iteration, calculate \sum_i{matrix^n/n!}
     for(float i = 0; i > 10; i++){
         partial_result = matrix;
         result = result + partial_result;
         cout << partial_result << endl;
-        //multiply the resulting 
-         
+        //multiply the resulting
+
         // To Do:
            // find out how long each operation takes
            // or find some way to decide how accurate you want to be
     }
     cout << result << endl;
-    
+
     int y = 3;
     int powers_test = powers();
     return powers_test;
     */
 }
-
-int main(){gfort
-    // matrix size
-    int iterations = 15;
-    int matrix = 3;
-    int n = 1;
-    cout << expo(matrix, iterations, n) << endl;
-//    matrix_exponential(matrix);
-    
-    // generate the elements
-    /*
-    random_seed();
-    double* elements = new double[n*n];
-    for(int i = 0; i = n*n; i++){
-        elements[i] = ran(10);
-    }
-    
-    // add the elements to the matrix
-    LaGenMatDouble matrix_nn = LaGenMatDouble(elements, n, n, row_ordering = true);
-    delete [] elements
-    
-    // test: print the lattice
-    cout << "Matrix:" << endl << matrix_nn << endl;
-    
-    // diagonalise the matrix
-    LaGenMatDouble diag = LaGenMatDouble( n, n );
-    diag = spins.diag();
-    
-    // test: print the diagonalised lattice
-    cout << "Diagonal matrix from spins" << endl << diag << endl;
-    */
-    
-    // calculate the exponential
-
-    
-//     this is the matrix to exponentiate
-//     to do this, we need to:
-//        find the eigenvalues and eigenvectors
-//        construct matrices with:
-//           the eigen values along the diagonal
-//           the corresponding eigenvectors in each column
-//              U^\dagger is the matrix with the eigenvectors along the columns
-//              U is the conjugate matrix
-//        in a for loop, calculate powers of this matrix
-//           in this, you only have to calculate powers of the exponential matrix
-//          
-//     then what do you have to do?
-
+long double exp_step(const long double number, const double step){
+  double result;
+  double A;
+  double B;
+  if(step <= 0){
+    cout << "step (0): 1 " << endl;
+    return number;
+  }else{
+      A = exp_step(number, step - 1);
+      B = number / step;
+      //cout << "step (" << step << "): ";
+      //cout << number << " / " << step << " = " << B << endl;
+      result = B * A;
+      //cout << B << " * " << A << " = " << result << endl;
+      return result;
+  }
+}
+long double recursive_exponential(const long double number, int iterations){
+  double result = 0;
+  long double step;
+  cout << result;
+  for(int i = 0; i < iterations; i ++){
+    step = exp_step(number, i);
+    cout << result << " + " << step;
+    result += step;
+    cout << " = " << result << endl;
+  }
+  return result;
 }
 
+/* Working Version */
+double exponent(const double number, const int iterations){
+    double A, B = 1, C = 1;
+    for(int n = 1; n <= iterations; n++){
+        for(int i = 1; i <= n; i++){
+            A = number / i;
+            B *= A;
+        }
+        C += B;
+        B = 1;
+    }
+    return C;
+}
 
+int main(){
+    int iterations = 1000, n = 1;
+    double number = 5;
+    cout.precision(50);
+    cout << "better: " << exponent(number, iterations) << endl;
+}
