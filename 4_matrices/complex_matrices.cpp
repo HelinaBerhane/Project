eb8137d7d00ab45c6b507de7b89a85d68aa9fb6c
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-01-21 21:37:52 +0000

    testing

--- 4_matrices/complex_matrices.cpp
+++ 4_matrices/complex_matrices.cpp
@@ -348,19 +348,19 @@ void test_matrix_multiplication(const int matrix_size, const int max_rand){
     /* initial result */
     LaGenMatComplex result = LaGenMatComplex::zeros(matrix_size, matrix_size);
     /* A * B */
-    Blas_Mat_Mat_Mult(matrixA, matrixB, result, false, false, alpha, beta);
+    Blas_Mat_Mat_Mult(matrixA, matrixB, result);
     print_matrix(result, "Matrix A * Matrix B");
     /* A^T * B */
     result = LaGenMatComplex::zeros(matrix_size, matrix_size);
     print_matrix(result, "0");
-    Blas_Mat_Trans_Mat_Mult(matrixA, matrixB, result, false, false, alpha, beta);
+    Blas_Mat_Trans_Mat_Mult(matrixA, matrixB, result);
     print_matrix(result, "Matrix A^T * Matrix B");
     /* A * B^T */
     print_matrix(matrixA, "Matrix A");
     print_matrix(matrixB, "Matrix B");
     result = LaGenMatComplex::zeros(matrix_size, matrix_size);
     print_matrix(result, "0");
-    Blas_Mat_Mat_Trans_Mult(matrixA, matrixB, result, false, false, alpha, beta);
+    Blas_Mat_Mat_Trans_Mult(matrixA, matrixB, result);
     print_matrix(result, "Matrix A * Matrix B^T");
 }
 void test_idenpotent_exponential(){
