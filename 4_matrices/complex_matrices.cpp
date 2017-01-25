5ef2c61a7acdf8068503f789aac9b3bd43da1244
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-01-25 20:12:38 +0000

    testing

--- 4_matrices/complex_matrices.cpp
+++ 4_matrices/complex_matrices.cpp
@@ -96,7 +96,7 @@ void generate_H(const int matrix_size, LaGenMatComplex& hamiltonian){//working
             cout.width(3);
             cout << abs(i-j);
             if(abs(i-j) == 1 || abs(i-j) == matrix_size - 1){
-                elements[n].r = 1;
+                elements[n].r = -1;
             }else{
                 elements[n].r = 0;
             }
