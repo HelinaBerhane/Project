d4954899bc480677880616d1df9f231f4b5283e1
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-02-11 18:15:29 +0000

    random int - bug fixes

--- 4_matrices/complex_matrices.cpp
+++ 4_matrices/complex_matrices.cpp
@@ -615,15 +615,25 @@ void test_random_int(){
         test = random_int(max_rand);
         switch(test){
             case 0: zero++;
+                break;
             case 1: one++;
+                break;
             case 2: two++;
+                break;
             case 3: three++;
+                break;
             case 4: four++;
+                break;
             case 5: five++;
+                break;
             case 6: six++;
+                break;
             case 7: seven++;
+                break;
             case 8: eight++;
+                break;
             case 9: nine++;
+                break;
         }
     }
     cout << "0: " << zero << endl;
