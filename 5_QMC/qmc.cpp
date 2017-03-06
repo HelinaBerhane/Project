e6a72872ac12c5cca0520f505ed18b06fb3810c1
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-03-06 14:51:02 +0000

    implement V - fixed matrix size

--- 5_QMC/qmc.cpp
+++ 5_QMC/qmc.cpp
@@ -29,7 +29,9 @@ void print_sites(const double array[], const int array_size){
 void print_vector(const LaVectorComplex& vector, const string name){
     cout << name << ":" << endl << vector << endl;
 }
+
 /* -- Generation -- */
+
 // Random Numbers
 int random_spin(){
     random_device rd;
@@ -43,6 +45,7 @@ double random_probability(){
     std::uniform_real_distribution<> dist(0, 1);
     return dist(gen);
 }
+
 // Structures
 void generate_lattice_array(double array[], const int array_size){
     for(int i = 0; i < array_size; i++){
@@ -50,45 +53,47 @@ void generate_lattice_array(double array[], const int array_size){
     }
 }
 
-
 /* -- Calculation -- */
+
 // Initial Parameters
-void initial_parameter_calculation(const double U, const double beta, double& lambda, double& delta_tau, int& time_slices){
+void initial_parameter_calculation(const double U, const double beta, double& lambda, double& delta_tau, int& time_size){
     // delta_tau = sqrt(0.125 / U);            // by convension
     lambda = acosh(exp(sqrt(0.125*U)/2));   // by definition
-    time_slices = ceil(beta / lambda);      // by definition
-    delta_tau = beta / time_slices;
+    time_size = ceil(beta / lambda);      // by definition
+    delta_tau = beta / time_size;
 }
+
 // Weights
-void generate_H(const int matrix_size, LaGenMatDouble& H){
+void generate_H(const int lattice_size, LaGenMatDouble& H){
     /* initialise everything */
-    int matrix_volume = matrix_size * matrix_size, i;
+    int matrix_volume = lattice_size * lattice_size, i;
     double elements[matrix_volume];
 
     /* generate the matrix */
-    for(int row = 0; row < matrix_size; row++){
-        for (int column = 0; column < matrix_size; column++) {
-            i = (matrix_size * row) + column;
-            if(abs(row - column) == 1 || abs(row - column) == matrix_size - 1){
+    for(int row = 0; row < lattice_size; row++){
+        for (int column = 0; column < lattice_size; column++) {
+            i = (lattice_size * row) + column;
+            if(abs(row - column) == 1 || abs(row - column) == lattice_size - 1){
                 elements[i] = -1;
             }else{
                 elements[i] = 0;
             }
         }
     }
-    H = LaGenMatDouble(elements, matrix_size, matrix_size, false);
+    H = LaGenMatDouble(elements, lattice_size, lattice_size, false);
 
     /* print the matrix */
     print_matrix(H, "H");
 }
+
 void test_H(){
     /* initialise everything */
-    int matrix_size = 5;
+    int lattice_size = 5;
     LaGenMatDouble H;
-    LaVectorComplex eigenvalues = LaVectorComplex(matrix_size);
-    LaGenMatDouble eigenvectors = LaGenMatDouble::zeros(matrix_size, matrix_size);
+    LaVectorComplex eigenvalues = LaVectorComplex(lattice_size);
+    LaGenMatDouble eigenvectors = LaGenMatDouble::zeros(lattice_size, lattice_size);
     /* generate matrix */
-    generate_H(matrix_size, H);
+    generate_H(lattice_size, H);
     print_matrix(H);
     /* calculate eigenstuff */
     LaEigSolve(H, eigenvalues, eigenvectors);
@@ -119,11 +124,11 @@ void test_lattice_array_generation(){
 }
 void test_parameter_calculation(){
     double beta = 10, U = 1, lambda, delta_tau;
-    int time_slices;
-    initial_parameter_calculation(U, beta, lambda, delta_tau, time_slices);
+    int time_size;
+    initial_parameter_calculation(U, beta, lambda, delta_tau, time_size);
     cout << "lambda = " << lambda << endl;
     cout << "delta tau = " << delta_tau << endl;
-    cout << "time slices = " << time_slices << endl;
+    cout << "no of time slices = " << time_size << endl;
 }
 void test_matrix_storage(){
     /* initialise everything */
@@ -157,29 +162,31 @@ void array_to_diag(const double array[], const int len, LaGenMatDouble& diag){
         diag(i, i) = array[i];
     }
 }
-void V_calculation(const double time_slice[], const int time_size, const double U, const double lambda, const double sigma, const double delta_tau, LaGenMatDouble& V){//should be working
+void V_calculation(const double time_slice[], const int lattice_size, const double U, const double lambda, const double sigma, const double delta_tau, LaGenMatDouble& V){
+
     /* initialise everything */
-    double V_elements[time_size], mu = 0;
-    /* V_ii = lambda sigma s_l / delta_tau + (mu - U/2) */
-    for(int i = 0; i < time_size; i++){
-        V_elements[i] = lambda * sigma * time_slice[i] / delta_tau + mu - U/2;
+    double V_elements[lattice_size], mu = 0;
+
+    /* calculate V */
+    for(int l = 0; l < lattice_size; l++){
+        V_elements[l] = lambda * sigma * time_slice[l] / delta_tau + mu - U/2;
     }
     /* save to diagonal matrix */
-    array_to_diag(V_elements, time_size, V);
+    array_to_diag(V_elements, lattice_size, V);
 }
 void test_V_generation(){//should work
 
     /* initialise everything */
-    int matrix_size = 5, time_slices;
-    double time_slice[matrix_size], U = 1, beta = 10, lambda, sigma, delta_tau;
-    LaGenMatDouble V = LaGenMatDouble::zeros(matrix_size, matrix_size);
+    int lattice_size = 5, time_slices;
+    double time_slice[lattice_size], U = 1, beta = 10, lambda, sigma, delta_tau;
+    LaGenMatDouble V = LaGenMatDouble::zeros(lattice_size, lattice_size);
 
     /* calculate initial parameters */
     initial_parameter_calculation(U, beta, lambda, delta_tau, time_slices);
 
     /* generate the time_slice */
-    generate_lattice_array(time_slice, time_slices);
-    V_calculation(time_slice, matrix_size, U, lambda, sigma, delta_tau, V);
+    generate_lattice_array(time_slice, lattice_size);
+    V_calculation(time_slice, lattice_size, U, lambda, sigma, delta_tau, V);
 
     /* print result */
     print_matrix(V, "V");
