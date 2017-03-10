95505f8f329133013e025a42e093aa1d8a4b62ea
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-03-10 19:00:49 +0000

    O - fixed output

--- 5_General/general.cpp
+++ 5_General/general.cpp
@@ -407,6 +407,7 @@ void O_calculation_v(const LaGenMatComplex& lattice, const int lattice_size, con
         isolate_row(lattice, lattice_size, t, slice);
         print_array(slice, lattice_size, "slice");
         B_calculation_v(slice, lattice_size, U, lambda, sigma, delta_tau, B);
+        print_matrix(B, "B");
         matrix_product(O, B);
         print_matrix(O, "product");
     }
