2b89db2f46358fb5bae48a1da7c6fe3c9e1fb03c
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-03-12 00:04:10 +0000

    increasing U - bug fixes

--- 5_General/general.cpp
+++ 5_General/general.cpp
@@ -811,7 +811,7 @@ void sweep_lattice(LaGenMatComplex& lattice, const int lattice_size, const int t
         }
     }
 }
-void sweep_lattice_v(LaGenMatComplex& lattice, const int lattice_size, const int time_size, const double U, const double lambda, const double delta_tau, const int iterations, int* acceptance, int& rejection){
+void sweep_lattice_v(LaGenMatComplex& lattice, const int lattice_size, const int time_size, const double U, const double lambda, const double delta_tau, const int iterations, int& acceptance, int& rejection){
     /* Plan */
 
         /* Input */
