9615504cbb2b5a8a82bbe0a2ac5b815e28d7c2f2
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-03-17 11:45:51 +0000

    bash scripts - testing

--- 6_Improved/save_results.sh
+++ 6_Improved/save_results.sh
@@ -3,7 +3,12 @@ message=$1
 if [[ -n "$message" ]]; then
     mkdir ../Tests/"$message"/
     mv U* ../Tests/"$message"/
+    ls ../Tests/"$message"/
 else
     mv U* ../Tests/
+    ls ../Tests/
 fi
+git add *
+git commit -m "return results"
+git push origin master
 ls
