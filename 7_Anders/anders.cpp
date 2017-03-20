f9377427da170135ad32cad13e7e806904f11a4b
Helina Berhane <helina.berhane@hotmail.co.uk>
2017-03-20 23:31:03 +0000

    overnight testing

--- 7_Anders/anders.cpp
+++ 7_Anders/anders.cpp
@@ -1274,20 +1274,79 @@ void test_increasing_U(){
 void test_increasing_mu(){
     /* initialise everything */
     int lattice_size = 5, time_size, iterations = 5000;
-    double U = 5, beta = 8, lambda, delta_tau, mu;
+    double U, beta = 5.76, lambda, delta_tau, mu;
     string asf = "average_spin";
     /* generate initial conditions */
     initial_parameter_calculation(U, beta, lambda, delta_tau, time_size);
     print_initial_parameters(U, beta, lambda, delta_tau, mu, time_size, lattice_size, iterations);
     /* plot mu */
-    for(double i = 0; i < 48; i++){
+    U = 0.5;
+    for(double i = 0; i < 32; i++){
         LaGenMatComplex lattice = LaGenMatComplex::zeros(lattice_size, time_size);
         generate_lattice(lattice_size, time_size, lattice);
-        mu = i * U / 16;
+        mu = i * U / 8;
         cout << "mu = " << mu << endl;
         /* sweep across the lattice */
         sweep_lattice(lattice, lattice_size, time_size, U, beta, lambda, delta_tau, mu, iterations);
     }
+    // U = 1;
+    // for(double i = 0; i < 32; i++){
+    //     LaGenMatComplex lattice = LaGenMatComplex::zeros(lattice_size, time_size);
+    //     generate_lattice(lattice_size, time_size, lattice);
+    //     mu = i * U / 8;
+    //     cout << "mu = " << mu << endl;
+    //     /* sweep across the lattice */
+    //     sweep_lattice(lattice, lattice_size, time_size, U, beta, lambda, delta_tau, mu, iterations);
+    // }
+    U = 1.5;
+    for(double i = 0; i < 32; i++){
+        LaGenMatComplex lattice = LaGenMatComplex::zeros(lattice_size, time_size);
+        generate_lattice(lattice_size, time_size, lattice);
+        mu = i * U / 8;
+        cout << "mu = " << mu << endl;
+        /* sweep across the lattice */
+        sweep_lattice(lattice, lattice_size, time_size, U, beta, lambda, delta_tau, mu, iterations);
+    }
+    // U = 2;
+    // for(double i = 0; i < 32; i++){
+    //     LaGenMatComplex lattice = LaGenMatComplex::zeros(lattice_size, time_size);
+    //     generate_lattice(lattice_size, time_size, lattice);
+    //     mu = i * U / 8;
+    //     cout << "mu = " << mu << endl;
+    //     /* sweep across the lattice */
+    //     sweep_lattice(lattice, lattice_size, time_size, U, beta, lambda, delta_tau, mu, iterations);
+    // }
+    U = 3.5;
+    for(double i = 0; i < 32; i++){
+        LaGenMatComplex lattice = LaGenMatComplex::zeros(lattice_size, time_size);
+        generate_lattice(lattice_size, time_size, lattice);
+        mu = i * U / 8;
+        cout << "mu = " << mu << endl;
+        /* sweep across the lattice */
+        sweep_lattice(lattice, lattice_size, time_size, U, beta, lambda, delta_tau, mu, iterations);
+    }
+    // U = 4;
+    // for(double i = 0; i < 32; i++){
+    //     LaGenMatComplex lattice = LaGenMatComplex::zeros(lattice_size, time_size);
+    //     generate_lattice(lattice_size, time_size, lattice);
+    //     mu = i * U / 8;
+    //     cout << "mu = " << mu << endl;
+    //     /* sweep across the lattice */
+    //     sweep_lattice(lattice, lattice_size, time_size, U, beta, lambda, delta_tau, mu, iterations);
+    // }
+    U = 4.5;
+    for(double i = 0; i < 32; i++){
+        LaGenMatComplex lattice = LaGenMatComplex::zeros(lattice_size, time_size);
+        generate_lattice(lattice_size, time_size, lattice);
+        mu = i * U / 8;
+        cout << "mu = " << mu << endl;
+        /* sweep across the lattice */
+        sweep_lattice(lattice, lattice_size, time_size, U, beta, lambda, delta_tau, mu, iterations);
+    }
+    // 0.500	5.76
+    //1.000	5.76
+    // 2.000	5.76
+    // 4.500	5.76
 }
 
 /* ------ TO TEST ------ */
@@ -1351,5 +1410,5 @@ void update_greens_function(){
 
 /* ------ Main QMC Program ------ */
 int main(){
-    test_sweep();
+    test_increasing_mu();
 }
